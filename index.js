45067b94f1ad9691dd13bea5c3cafb703fecdd20
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-24 13:26:37 +0800

    fixed for wx

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import BluetoothPrinter from './utils/bluetooth_printer'
 import BluetoothWeigher from './utils/bluetooth_weigher'
 import { PrintPOS, PrintCPCL, PrintPic, PrintCommand } from 'xcprinter'
-import { TextDecoder } from 'text-encoding'
+import { TextDecoder, TextEncoder } from 'text-encoding'
 
 // 关键步骤：显式挂载到全局作用域，确保主小程序调用时能跨作用域找到
 if (typeof globalThis !== 'undefined') {
   globalThis.TextDecoder = globalThis.TextDecoder || TextDecoder
+  globalThis.TextEncoder = globalThis.TextEncoder || TextEncoder
 } else if (typeof wx !== 'undefined') {
   wx.TextDecoder = wx.TextDecoder || TextDecoder
+  wx.TextEncoder = wx.TextEncoder || TextEncoder
 } else if (typeof window !== 'undefined') {
   window.TextDecoder = window.TextDecoder || TextDecoder
+  window.TextEncoder = window.TextEncoder || TextEncoder
 }
 
 
